d343311ea002d3ae92bac32de87c6faacd29dc51
zenghao <1054092535@qq.com>
2024-10-13 23:58:54 +0800

    feat✨: 运行微队列任务

--- handwrite-promise.js
+++ handwrite-promise.js
@@ -5,6 +5,29 @@ const promiseState = {
     'REJECTED': 'rejected'
 }
 
+/**
+ * 运行一个微队列任务
+ * 把传递的函数放到微队列中
+ * @param {Function} callback
+ */
+function runMicroTask(callback) {
+    // 判断node环境
+    // 为了避免「变量未定义」的错误，这里最好加上前缀globalThis
+    // globalThis是一个关键字，指代全局对象，浏览器环境为window，node环境为global
+    if (globalThis.process && globalThis.process.nextTick) {
+        process.nextTick(callback);
+    } else if (globalThis.MutationObserver) {
+        const p = document.createElement('p');
+        const observer = new MutationObserver(callback);
+        observer.observe(p, {
+            childList: true, // 观察该元素内部的变化
+        });
+        p.innerHTML = '1';
+    } else {
+        setTimeout(callback, 0);
+    }
+}
+
 class MyPromise {
 
     /**
